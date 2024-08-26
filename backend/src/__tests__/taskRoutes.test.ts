1cb4a42cf30f7bc599fa73b69c463687cb399549
Matt Johnston <mattjohnston2000@gmail.com>
2024-08-26 18:55:36 +0100

    adds more thorough testing for endpoints and seperates some validation logic into models

--- backend/src/__tests__/taskRoutes.test.ts
+++ backend/src/__tests__/taskRoutes.test.ts
@@ -1,13 +1,13 @@
 import request from "supertest";
-import express from "express";
+import express, { Express } from "express";
 import taskRoutes from "../routes/taskRoutes";
 import dynamoDB from "../dynamodb";
 
-jest.mock("../dynamodb");
-
-const app = express();
+const app: Express = express();
 app.use(express.json());
 app.use("/tasks", taskRoutes);
+
+jest.mock("../dynamodb");
 
 describe("Task API", () => {
   beforeEach(() => {
@@ -16,126 +16,182 @@ describe("Task API", () => {
 
   describe("createTask()", () => {
     it("should return 201 status", async () => {
-      const postMock = jest.fn().mockReturnValue({
+      (dynamoDB.put as jest.Mock).mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
 
-      (dynamoDB.put as jest.Mock).mockImplementation(postMock);
-
       const response = await request(app).post("/tasks").send({
-        id: "1",
+        taskId: "1",
         title: "Test Task",
         status: "pending",
       });
 
       expect(response.status).toBe(201);
     });
+
     it("should return correct message", async () => {
-      const postMock = jest.fn().mockReturnValue({
+      (dynamoDB.put as jest.Mock).mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
 
-      (dynamoDB.put as jest.Mock).mockImplementation(postMock);
-
       const response = await request(app).post("/tasks").send({
-        id: "1",
+        taskId: "1",
         title: "Test Task",
         status: "pending",
       });
 
       expect(response.body.message).toBe("Task created successfully");
     });
-    it("should call function one time when succesfully creating a task", async () => {
-      const postMock = jest.fn().mockReturnValue({
+
+    it("should call DynamoDB put method once when creating a task", async () => {
+      const putMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
 
-      (dynamoDB.put as jest.Mock).mockImplementation(postMock);
+      (dynamoDB.put as jest.Mock).mockImplementation(putMock);
 
-      const response = await request(app).post("/tasks").send({
-        id: "1",
+      await request(app).post("/tasks").send({
+        taskId: "1",
         title: "Test Task",
         status: "pending",
       });
 
-      expect(postMock).toHaveBeenCalledTimes(1);
+      expect(putMock).toHaveBeenCalledTimes(1);
     });
 
     it("should return an error when task creation fails", async () => {
-      const postMock = jest.fn().mockReturnValue({
+      (dynamoDB.put as jest.Mock).mockReturnValue({
         promise: jest.fn().mockRejectedValue(new Error("DynamoDB Error")),
       });
 
-      (dynamoDB.put as jest.Mock).mockImplementation(postMock);
-
       const response = await request(app).post("/tasks").send({
-        id: "1",
+        taskId: "1",
         title: "Test Task",
         status: "pending",
       });
 
       expect(response.body.error).toBe("DynamoDB Error");
     });
-    it("should return a 500 status", async () => {
-      const postMock = jest.fn().mockReturnValue({
+
+    it("should return a 500 status when unsuccessful", async () => {
+      (dynamoDB.put as jest.Mock).mockReturnValue({
         promise: jest.fn().mockRejectedValue(new Error("DynamoDB Error")),
       });
 
-      (dynamoDB.put as jest.Mock).mockImplementation(postMock);
-
       const response = await request(app).post("/tasks").send({
-        id: "1",
+        taskId: "1",
         title: "Test Task",
         status: "pending",
       });
 
       expect(response.status).toBe(500);
     });
+
+    it("should return 400 status when required fields are missing", async () => {
+      const response = await request(app).post("/tasks").send({
+        taskId: "",
+      });
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe(
+        "Task ID is required, Title is required, Status is required"
+      );
+    });
   });
 
   describe("getTasks()", () => {
     it("should return a 200 status on successful request", async () => {
-      const scanMock = jest.fn().mockReturnValue({
+      (dynamoDB.scan as jest.Mock).mockReturnValue({
         promise: jest.fn().mockResolvedValue({
-          Items: [{ id: "1", title: "Test Task", status: "pending" }],
+          Items: [{ taskId: "1", title: "Test Task", status: "pending" }],
         }),
       });
-
-      (dynamoDB.scan as jest.Mock).mockImplementation(scanMock);
 
       const response = await request(app).get("/tasks");
 
       expect(response.status).toBe(200);
-      expect(scanMock).toHaveBeenCalledTimes(1);
     });
-    it("should return correct number of tasks", async () => {
-      const scanMock = jest.fn().mockReturnValue({
+
+    it("should return an empty array when no tasks are present", async () => {
+      (dynamoDB.scan as jest.Mock).mockReturnValue({
         promise: jest.fn().mockResolvedValue({
-          Items: [{ id: "1", title: "Test Task", status: "pending" }],
+          Items: [],
         }),
       });
-
-      (dynamoDB.scan as jest.Mock).mockImplementation(scanMock);
 
       const response = await request(app).get("/tasks");
 
-      expect(response.body.length).toBe(1);
+      expect(response.body).toEqual([]);
     });
-    it("should call function one time when succesfully retrieving tasks", async () => {
+
+    it("should call DynamoDB scan method once when retrieving tasks", async () => {
       const scanMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({
-          Items: [{ id: "1", title: "Test Task", status: "pending" }],
+          Items: [{ taskId: "1", title: "Test Task", status: "pending" }],
         }),
       });
 
       (dynamoDB.scan as jest.Mock).mockImplementation(scanMock);
+
+      await request(app).get("/tasks");
 
       expect(scanMock).toHaveBeenCalledTimes(1);
     });
   });
 
   describe("updateTask()", () => {
-    it("should update a task", async () => {
+    it("should return 200 status when successfully updating a task", async () => {
+      (dynamoDB.update as jest.Mock).mockReturnValue({
+        promise: jest.fn().mockResolvedValue({
+          Attributes: { status: "completed" },
+        }),
+      });
+
+      const response = await request(app).put("/tasks/1").send({
+        status: "completed",
+      });
+
+      expect(response.status).toBe(200);
+    });
+
+    it("should return updated status when task is updated successfully", async () => {
+      (dynamoDB.update as jest.Mock).mockReturnValue({
+        promise: jest.fn().mockResolvedValue({
+          Attributes: { status: "completed" },
+        }),
+      });
+
+      const response = await request(app).put("/tasks/1").send({
+        status: "completed",
+      });
+
+      expect(response.body.status).toBe("completed");
+    });
+
+    it("should return 400 status when no status field is provided", async () => {
+      const response = await request(app).put("/tasks/1").send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe("Status is required");
+    });
+
+    it("should return 404 status when task ID is invalid", async () => {
+      (dynamoDB.update as jest.Mock).mockReturnValue({
+        promise: jest.fn().mockRejectedValue(new Error("Task not found")),
+      });
+
+      const response = await request(app).put("/tasks/invalid-id").send({
+        status: "completed",
+      });
+
+      console.log(response.status, "<<<");
+      console.log(response.body.error, "<<<");
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("Task not found");
+    });
+
+    it("should call DynamoDB update method once when updating a task", async () => {
       const updateMock = jest.fn().mockReturnValue({
         promise: jest.fn().mockResolvedValue({
           Attributes: { status: "completed" },
@@ -144,28 +200,46 @@ describe("Task API", () => {
 
       (dynamoDB.update as jest.Mock).mockImplementation(updateMock);
 
-      const response = await request(app).put("/tasks/1").send({
+      await request(app).put("/tasks/1").send({
         status: "completed",
       });
 
-      expect(response.status).toBe(200);
-      expect(response.body.status).toBe("completed");
       expect(updateMock).toHaveBeenCalledTimes(1);
     });
   });
+
   describe("deleteTask()", () => {
-    it("should delete a task", async () => {
-      const deleteMock = jest.fn().mockReturnValue({
+    it("should return 200 status and message when successfully deleting a task", async () => {
+      (dynamoDB.delete as jest.Mock).mockReturnValue({
         promise: jest.fn().mockResolvedValue({}),
       });
-
-      (dynamoDB.delete as jest.Mock).mockImplementation(deleteMock);
 
       const response = await request(app).delete("/tasks/1");
 
       expect(response.status).toBe(200);
       expect(response.body.message).toBe("Task deleted successfully");
-      expect(deleteMock).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return 404 status when task to delete does not exist", async () => {
+      (dynamoDB.delete as jest.Mock).mockReturnValue({
+        promise: jest.fn().mockRejectedValue(new Error("Task not found")),
+      });
+
+      const response = await request(app).delete("/tasks/invalid-id");
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("Task not found");
+    });
+
+    it("should return 500 status when delete operation fails", async () => {
+      (dynamoDB.delete as jest.Mock).mockReturnValue({
+        promise: jest.fn().mockRejectedValue(new Error("DynamoDB Error")),
+      });
+
+      const response = await request(app).delete("/tasks/1");
+
+      expect(response.status).toBe(500);
+      expect(response.body.error).toBe("DynamoDB Error");
     });
   });
 });
