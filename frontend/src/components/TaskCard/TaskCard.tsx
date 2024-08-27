fa13a5766af5f6c56c55f5f014c12fb1ed1c69d2
Matt Johnston <mattjohnston2000@gmail.com>
2024-08-27 02:15:51 +0100

    changes status selection to drop-down

--- frontend/src/components/TaskCard/TaskCard.tsx
+++ frontend/src/components/TaskCard/TaskCard.tsx
@@ -15,7 +15,7 @@ const TaskCard: React.FC<TaskCardProps> = ({title, status, taskId, onDelete}) =>
         <Card className={styles.card}>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
-                <Card.Text>{status}</Card.Text>
+                <Card.Text>Status: {status}</Card.Text>
                 <div className={styles.buttonWrapper}>
                 <Button variant="success" className={styles.updateButton}>Update</Button>
                 <Button variant="danger" onClick={() => onDelete(taskId)}>Delete</Button>
